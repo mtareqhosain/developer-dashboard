f0a27b337250bdbc1423160e6aa5a4933b1c5d24
Maruf Ahmed <maruf_m1@yahoo.com>
2020-08-20 16:11:26 +0600

    Removed build foler from gitignore

---
+++ build/precache-manifest.bf067e00338dc947069cdfe6ea870b99.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "06f99cf0ad77c6e68b3dd7775c97a413",
+    "url": "/index.html"
+  },
+  {
+    "revision": "b200fe06bfabe7b76f59",
+    "url": "/static/css/main.de6846f6.chunk.css"
+  },
+  {
+    "revision": "e7145641301b7c07a3d9",
+    "url": "/static/js/2.b654e504.chunk.js"
+  },
+  {
+    "revision": "0ec85062ca85c8586b84c2932d55dba1",
+    "url": "/static/js/2.b654e504.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b200fe06bfabe7b76f59",
+    "url": "/static/js/main.fb8342cf.chunk.js"
+  },
+  {
+    "revision": "d22dac535eec5582969c",
+    "url": "/static/js/runtime-main.c2003f65.js"
+  },
+  {
+    "revision": "9bcab304c34d445c91381031f71dd560",
+    "url": "/static/media/auth_init.9bcab304.png"
+  }
+]);
