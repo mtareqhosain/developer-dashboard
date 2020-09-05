5125c53d670228ecda2bca0c05a4e80e833c2d95
Maruf Ahmed <maruf_m1@yahoo.com>
2020-09-05 15:05:52 +0600

    init

---
+++ build/precache-manifest.046aebc3e5b23e3a4f853361e4b89741.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4925177300542610b1453fc632575555",
+    "url": "/index.html"
+  },
+  {
+    "revision": "0289e4766cf9d560eb97",
+    "url": "/static/css/main.ea662aef.chunk.css"
+  },
+  {
+    "revision": "fc9305f230993aef14f5",
+    "url": "/static/js/2.7788e580.chunk.js"
+  },
+  {
+    "revision": "0ec85062ca85c8586b84c2932d55dba1",
+    "url": "/static/js/2.7788e580.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "0289e4766cf9d560eb97",
+    "url": "/static/js/main.b1cd2638.chunk.js"
+  },
+  {
+    "revision": "d22dac535eec5582969c",
+    "url": "/static/js/runtime-main.c2003f65.js"
+  },
+  {
+    "revision": "9bcab304c34d445c91381031f71dd560",
+    "url": "/static/media/auth_init.9bcab304.png"
+  }
+]);
