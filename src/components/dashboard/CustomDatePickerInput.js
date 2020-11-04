711edfbb87d54f5bf1bfdd7348a15782f1259cf1
Tareq <tareqtherocker@gmail.com>
2020-11-04 11:48:14 +0600

    date picker style updated v4

--- src/components/dashboard/CustomDatePickerInput.js
+++
@@ -1,7 +0,0 @@
-const CustomDatePickerInput = ({ value, onClick }) => (
-    <button className='example-custom-input' onClick={onClick}>
-        {value}
-    </button>
-);
-
-export default CustomDatePickerInput;
